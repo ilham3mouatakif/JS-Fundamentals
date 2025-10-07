d092764607bd0fed82a09bc8527500f9e0680b16
ilham3mouatakif <mouatakif3ilham@gmail.com>
2025-10-07 12:03:15 +0000

    file

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,8 @@
 
 #!/usr/bin/node
-
-const firstArg = process.argv[2];
-
-if (firstArg === undefined) {
-    console.log("No argument");
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-    console.log(firstArg);
+  console.log(process.argv[2]);
 }
+
